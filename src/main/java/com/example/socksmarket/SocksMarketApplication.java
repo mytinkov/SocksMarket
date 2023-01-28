d078b696b197678ae730e31aa8738d6459ccbcd6
tinkov <vasy499@mail.ru>
2023-01-28 11:08:56 +0300

    Initial commit

---
+++ src/main/java/com/example/socksmarket/SocksMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.socksmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socksmarket/SocksMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socksmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
